cd9cb9c32eb60ff489d1354d948f9372d1af7862
theoneandonlyalfre2 <130306176+theoneandonlyalfre2@users.noreply.github.com>
2023-04-28 14:07:19 -0400

    Add files via upload
    Music now available

--- player.gd
+++ player.gd
@@ -1,13 +1,16 @@
 extends CharacterBody3D
 
 @onready var luz_var = %Luz_var
+@onready var audio_stream_player = $AudioStreamPlayer
+
+var Poder = preload("res://poder.tscn")
 var poder_activado = false
-var tiempo_poder_activado = 5
+var tiempo_poder_activado = 3
 var tiempo_espera = 3
 var poder_cargado = true
-var tiempo_espacio_prohibido = 2
+var tiempo_espacio_prohibido = 1
 const SPEED = 5.0
-const JUMP_VELOCITY = 2.5
+const JUMP_VELOCITY = 3
 const Alt_min = 1
 const Alt_max = 3
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -16,39 +19,15 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 
+
 func _physics_process(delta):
 	# Add the gravity.
-	if Input.is_action_just_pressed("poder") and poder_cargado:
-		poder_activado = true
-	if Alt_max < position.y or position.y < Alt_min:
-		luz_var.light_color = Color(1,0,0,0)
-		
-		tiempo_espacio_prohibido -= delta
-		if tiempo_espacio_prohibido < 0:
-			get_tree().change_scene_to_file("res://MI_theme/Menu_gameover.tscn")
-	if Alt_max > position.y and position.y > Alt_min:
-		tiempo_espacio_prohibido = 2
-		luz_var.light_color = Color(1,1,1,1)
-	if poder_activado:
-		luz_var.light_energy += 2*delta
-		tiempo_poder_activado -= delta
-	if tiempo_poder_activado < 0:
-		luz_var.light_energy = 1
-		poder_activado = false
-		poder_cargado = false
-		tiempo_poder_activado = 5
-		tiempo_espera = 3
-	if not poder_cargado:
-		if tiempo_espera < 0:
-			poder_cargado = true
-		else:
-			tiempo_espera -= delta
-		
 	velocity.y -= gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
+		audio_stream_player.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -63,10 +42,9 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
 	
-	
-	
-	
+
 	
 	
 	
