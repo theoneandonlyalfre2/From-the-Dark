90508ba7952231571bb6f3fea6c60498f7ca365f
matiasAguirreE <125716100+matiasAguirreE@users.noreply.github.com>
2023-04-28 23:14:02 -0400

    DOCS: Added documentation to the script player.gd

--- player.gd
+++ player.gd
@@ -1,30 +1,55 @@
 extends CharacterBody3D
 
+## Version: 1.1 |
+## This script defines the behavior of the bat.
+##
+## This script defines how the bat can fly, how the bat can move and how the bat can use his echolocation hability.
+
+# Load references
+
+## Load reference to a Luz_var node.
 @onready var luz_var = %Luz_var
+## Load reference to an AudioStreamPlayer node.
 @onready var audio_stream_player = $AudioStreamPlayer
 
+## Load a reference to a poder.tscn scene.
 var Poder = preload("res://poder.tscn")
+
+# Define variables to track the state of the player's abilities and various time intervals.
+
+## Power of the bat activated.
 var poder_activado = false
+## Time of the power been activated.
 var tiempo_poder_activado = 3
+## Time of wait.
 var tiempo_espera = 3
+## Power of the bat loaded.
 var poder_cargado = true
+## Time of space forbidden.
 var tiempo_espacio_prohibido = 1
+
+# Define constants for movement and jumping.
+
+## Speed of the bat.
 const SPEED = 5.0
+## Jump velocity of the bat.
 const JUMP_VELOCITY = 3
+## Min of height.
 const Alt_min = 1
+## Max of height.
 const Alt_max = 3
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
+## Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+# Define the methods.
 
-
-
-
+## Physics of the bat.
 func _physics_process(delta):
 	# Add the gravity.
 	velocity.y -= gravity * delta
 
-	# Handle Jump.
+	# Handle jumping when the "ui_accept" action is pressed.
 	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 		audio_stream_player.play()
@@ -34,6 +59,7 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	# Move the player based on the input direction or decelerate if no input is detected.
 	if direction:
 		velocity.x = -direction.x * SPEED
 		velocity.z = direction.y * SPEED
@@ -41,11 +67,5 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
+	# Move the player based on their current velocity.
 	move_and_slide()
-
-	
-
-	
-	
-	
-	
