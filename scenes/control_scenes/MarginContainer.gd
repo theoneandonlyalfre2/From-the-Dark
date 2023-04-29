7c7baa29ec596b55d547a2775683e69f17e33cbb
matiasAguirreE <125716100+matiasAguirreE@users.noreply.github.com>
2023-04-29 17:12:51 -0400

    DOCS: Added documentation to MarginContainer.gd

--- scenes/control_scenes/MarginContainer.gd
+++ scenes/control_scenes/MarginContainer.gd
@@ -1,15 +1,29 @@
 extends MarginContainer
+
+## Version: 1.1 |
+## Short description.
+##
+## Long description.
+
+# Load references.
+
+## Load reference to Start node.
 @onready var start = %Start
+## Load reference to Quit node.
 @onready var quit = %Quit
 
-# Called when the node enters the scene tree for the first time.
+# Define the methods that will be called when the node enters the scene tree for the first time.
+
+## Undefined.
 func _ready():
 	start.pressed.connect(_on_start_presssed)
 	quit.pressed.connect(_on_quit_presssed)
 
+## Undefined.
 func _on_start_presssed():
 	get_tree().change_scene_to_file("res://scenes/control_scenes/node_3d.tscn")
 	get_tree().paused = false
 	
+## Undefined.
 func _on_quit_presssed():
 	get_tree().quit()
