4410f3d388b2aea5055f86c9022c7a5719a8956b
matiasAguirreE <125716100+matiasAguirreE@users.noreply.github.com>
2023-07-02 19:45:31 -0400

    FEAT: Added rotation to the owl

--- models/characters/buho/Buho.gd
+++ models/characters/buho/Buho.gd
@@ -14,3 +14,5 @@ func _physics_process(_delta):
 
 func update_target_location(target_location):
 	nav_agent.set_target_position(target_location)
+	# Rotate owl
+	look_at(Vector3(target_location.x, global_transform.origin.y, target_location.z), Vector3.UP)
