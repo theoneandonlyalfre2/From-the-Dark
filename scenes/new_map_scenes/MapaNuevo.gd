bde1ba1c909448f92a092d3de6cc22058e9ed501
Canoso14 <cristian.conejeros.cont@gmail.com>
2023-07-09 17:13:03 -0400

    Win Condition
    Agregamos la condición de victoria

--- scenes/new_map_scenes/MapaNuevo.gd
+++ scenes/new_map_scenes/MapaNuevo.gd
@@ -5,6 +5,7 @@ extends Node3D
 @onready var gameover = $Characters/Buho/Pivot/GameOver
 @onready var owlanimation= $Characters/Buho/AnimationPlayer
 @onready var owllight= $Characters/Buho/ScreamerLight
+@onready var wincondition= $Mapa/NavigationRegion3D/Walls/HabitacionFinal/WinCondition
 
 func _physics_process(_delta):
 	pass #get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
@@ -24,3 +25,8 @@ func _on_buho_attacking(is_attacking):
 func _on_buho_spoting(is_spoting):
 	if is_spoting:
 		get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
+
+
+func _on_area_3d_body_entered(body):
+	if body.is_in_group("player"):
+		print("You've won this time")
